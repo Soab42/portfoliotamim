627001b8390f1efb0e5a74caf81e233eea380f35
Soab Mahmud Syfuddhin <108150913+Soab42@users.noreply.github.com>
2024-06-16 15:24:23 +0600

    Update lazyLoad.js

--- src/utiles/lazyLoad.js
+++ src/utiles/lazyLoad.js
@@ -1,6 +1,20 @@
-import {lazy} from 'react';
+// import {lazy} from 'react';
 
-export function lazyLoad(file){
-     return lazy(()=> import(/* @vite-ignore */`../components/${file}`))
+// export function lazyLoad(file){
+//      return lazy(()=> import(/* @vite-ignore */`../components/${file}`))
    
+// }
+
+const componentMap = {
+    about: () => import('../components/About'),
+    certificate: () => import('../components/Certificate'),
+    // Add other components here
+};
+
+export function lazyLoad(file) {
+    const importFunction = componentMap[file];
+    if (!importFunction) {
+        throw new Error(`Component ${file} not found`);
+    }
+    return lazy(importFunction);
 }
